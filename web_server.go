8d098e6bae6981f3e316440355c7897aca060e65
pegan-1 <peter.h.egan@gmail.com>
2021-08-17 07:48:10 -0500

    Added basic navigation for static pages
    Added basic navigation for static pages

--- web_server.go
+++ web_server.go
@@ -12,8 +12,8 @@ func (WebServer) listen() {
 	// Set up the route handlers
 	http.HandleFunc("/hello", helloHandler)
 
-	// Set up the form handler.
-	http.HandleFunc("/form", formHandler)
+	// Set up the admin handler...
+	http.HandleFunc("/redcard", redcardHandler)
 
 	// Set up the file server to serve the static web pages
 	fileServer := http.FileServer(http.Dir("./static"))
@@ -26,7 +26,7 @@ func (WebServer) listen() {
 }
 
 // Route Handlers
-// /hello
+// /hello - Placeholder for now.
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -41,16 +41,29 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello There!")
 }
 
-// Form Handlers (example for now)
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
+// Manages the redcard admin panel
+func redcardHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/redcard" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
 
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+	// Process 'Get' and 'Post' calls
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "static/redcard.html")
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseRedcardPost() err: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+		title := r.FormValue("blog_title")
+		post := r.FormValue("blog_body")
+		fmt.Fprintf(w, "Title = %s\n", title)
+		fmt.Fprintf(w, "Post = %s\n", post)
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
 }
