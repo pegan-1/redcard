0c1eabda5a486a6ab7ee70a164dca45fa9aac36c
pegan-1 <peter.h.egan@gmail.com>
2021-08-22 07:55:56 -0500

    Added new blog post to blog
    Able to post to the blog.

--- blog_engine.go
+++ blog_engine.go
@@ -4,14 +4,19 @@ Manages the blog for the redcard instance.
 
 @author  Peter Egan
 @since   2021-08-17
-@lastUpdated 2021-08-17
+@lastUpdated 2021-08-22
 
 Copyright (c) 2021 kiercam llc
 */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+)
 
 type blog_post struct {
 	title   string
@@ -19,23 +24,41 @@ type blog_post struct {
 }
 
 func (b blog_post) post() {
-	fmt.Println("Posting to the Blog!!!")
-	fmt.Println(b.title)
-	fmt.Println(b.content)
+	// Given a valid blog post, write the post to the blog file.
+	// 1) Read in the blog file...
+	blog, err := ioutil.ReadFile("static/blog.html")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return // Todo - better error handling.
+	}
 
-	// // Note: The blog does not exist until the first blog post.
-	// if _, err := os.Stat("/path/to/whatever"); err == nil {
-	// 	// path/to/whatever exists
-	// 	fmt.Println("Blog file already exists!!!")
+	// 2) Insert the blog post in the file ...
+	// a) Grab the datetime
+	currentTime := time.Now()
 
-	// } else if os.IsNotExist(err) {
-	// 	// path/to/whatever does *not* exist
-	// 	fmt.Println("The ")
+	// b) Covert the blog slice into a string
+	blogString := string(blog)
 
-	// } else {
-	// 	// Schrodinger: file may or may not exist. See err for details.
+	// c) Create the new HMTL for the blog post
+	newPostString := `<div class="post" id="placeholder">
+	<h2>%s</h2>
+	<h5>%s</h5>
+ 	<!-- <div class="fakeimg" style="height:200px;">Image</div> -->
+	<p>%s</p>
+	</div>`
+	newPost := fmt.Sprintf(newPostString, b.title, currentTime.Format("2006-January-02"), b.content)
 
-	// 	// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+	// d) Split the blog byte slice.
+	s := strings.SplitAfter(blogString, "<div class=\"row\">")
 
-	// }
+	// e) Recombine: slice1 + new post + slice2
+	newBlog := s[0] + "\n\t" + newPost + s[1]
+
+	// 3) Write out the new blog...
+	newBlogFile := []byte(newBlog)
+	errFile := ioutil.WriteFile("static/blog.html", newBlogFile, 0644)
+	if errFile != nil {
+		fmt.Printf("err: %v\n", errFile)
+		return // Todo - better error handling.
+	}
 }
