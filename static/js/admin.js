8684f5045a88c3f3e29ddb9f8a6aa95fe6f384cc
pegan-1 <peter.h.egan@gmail.com>
2021-09-06 08:19:16 -0500

    Added /static Web Elements to Source Control
    The /static web elements have been added to source control.

---
+++ static/js/admin.js
@@ -0,0 +1,77 @@
+/*
+admin.js
+Scripting for the admin panel.
+
+@author  Peter Egan
+@lastUpdated 2021-09-06
+
+Copyright (c) 2021 kiercam llc
+*/
+
+// Document Elements
+const editor = document.getElementById('editor');
+const blogTitle = document.getElementById('blog_title');
+const publishButton = document.getElementById('publish');
+
+//add the toolbar options
+let myToolbar= [
+    // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    ['bold', 'italic', 'underline', 'strike'],  
+    [{ 'color': [] }, { 'background': [] }], 
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }], 
+    ['blockquote', 'code-block'], 
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'align': [] }],         
+    // [{ 'font': [] }],
+    // [{ 'align': [] }],
+    // ['link'],        
+    ['image'], 
+    ['clean']         
+];
+
+let quill = new Quill('#editor', {
+    theme: 'snow',
+    modules: {
+        toolbar: {
+            container: myToolbar,
+            // handlers: {
+            //     // handlers object will be merged with default handlers object
+            //     'link': function(value) {
+            //         if (value) {
+            //             let urlLink = prompt('Enter the URL');
+            //             this.quill.format('link', urlLink);
+            //         } else {
+            //             this.quill.format('link', false);
+            //         }
+            //     } 
+            // }
+        },
+        imageResize: {
+            displaySize: true
+        },
+    },
+});
+
+// Set up event listeners
+publishButton.addEventListener('click',async _ => {
+    // Grab the editor content.
+    let content = quill.container.firstChild.innerHTML
+
+    // Send the blog post to the server
+    try{
+        // Construct the payload
+        let payload = {"title": blogTitle.value, "content":content};
+ 
+        // Send the  HTTP Request
+        let request = new XMLHttpRequest();
+        request.open("POST", '/admin')
+        request.setRequestHeader("Accept", "application/json");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(payload))
+        // window.location.href = "http://localhost.com/blog.html";
+    } catch(err) {
+      // TBD... How do I manage errors?
+      console.error(`Error: ${err}`);
+    }
+});
